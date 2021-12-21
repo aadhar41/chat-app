09e44bc521d834293e45260912a8c486f2ce940a
Aadhar gaur <aadhar41@gmail.com>
2021-12-21 20:48:49 +0530

    Chat application

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express'),
+        app = express(),
+        server = require("http").Server(app),
+        io = require("socket.io")(server);
+        usernames = [];
+        
+
+server.listen(process.env.PORT || 3000);
+
+console.log("Server running...");
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on("connection", function(socket) {
+    console.log("Socket Connected...");
+
+    socket.on("new user", function(data, callback) {
+        if (usernames.indexOf(data) != -1) {
+            callback(false);   
+        } else {
+            callback(true);
+            socket.username = data;
+            usernames.push(socket.username);
+            updateUsernames();
+        }
+    });
+
+    // Update Usernames
+    function updateUsernames(params) {
+        io.sockets.emit("usernames", usernames);
+    }
+
+    // Send Message
+    socket.on("send message", function(data) {
+        io.sockets.emit("new message", {msg: data.msg, user: socket.username, id: socket.id, count: data.count});
+    });
+
+    socket.on('typing', (data) => {
+        io.sockets.emit('typing', (socket.username));
+    });
+
+    // Disconnect
+    socket.on("disconnect", function(data) {
+       if (!socket.username) {
+           return;
+       } 
+
+       usernames.splice(usernames.indexOf(socket.username), 1);
+       updateUsernames();
+    });
+
+});
+
+
+
+
+
+
+
